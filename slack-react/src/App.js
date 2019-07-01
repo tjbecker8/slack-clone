3a497dbb1d325f485690f93cbc472d183e6c1e17
Tom Becker <tjbecker@me.com>
2019-07-01 15:14:13 +0700

    started with creating basic app

--- slack-react/src/App.js
+++ slack-react/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Sidebar from './sidebar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	//data
+	//functions
+	//render
+	render() {
+		return (
+			<div>
+				<Sidebar />
+				<h1>Hello</h1>
+				<p>Yo</p>
+			</div>
+		)
+	}
 }
+
+
 
 export default App;
---
+++ slack-react/src/sidebar.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+// import './App.css';
+
+class Sidebar extends Component {
+	//data
+	//functions
+	//render
+	render() {
+		return (
+			<div>
+			Sidebar
+			</div>
+		)
+	}
+}
+
+
+
+export default Sidebar;
