253e7b8d785e9167dd1129b607cd9f54df3d8218
Tom Becker <tjbecker@me.com>
2019-07-03 11:30:29 +0700

    setting up backend server

---
+++ slack-server/index.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+
+app.use(express.static(path.join(__dirname, 'client')))
+
+
+
+
+
+app.listen(process.env.PORT, () => {
+	console.log(`Server listening on port ${process.env.PORT}`);
+})
