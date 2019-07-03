bc11d24ddc5c28cb474b2016acb6a435c92f35b3
Tom Becker <tjbecker@me.com>
2019-07-03 09:54:06 +0700

    remove text after sending message

--- slack-react/src/NewMessage.js
+++ slack-react/src/NewMessage.js
@@ -16,13 +16,22 @@ class NewMessage extends Component {
 			text: e.target.value
 		})
 	}
+
+	clearMessage = () => {
+		this.setState({
+			text: ''
+		})
+	}
 	//render
 	render() {
 		return (
 			<div id="new-messages">
-				<form onSubmit= {(e) => this.props.createMessage(e, this.state.text)}>
+				<form onSubmit= {(e) => {
+						this.props.createMessage(e, this.state.text);
+						this.clearMessage()}
+					}>
 					<div className="input-group mb-3">
-						<input type="text" className="form-control" placeholder="new message" value={this.state.text} onChange={(e) => this.changeText(e)} />
+						<input type="text" className="form-control" placeholder="New Message" value={this.state.text} onChange={(e) => this.changeText(e)} />
 						<div className="input-group-append">
 							<button className="btn btn-success" type="submit" >Send</button>
 						</div>
